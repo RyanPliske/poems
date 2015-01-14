2a903d28c3a8ff291cd6d0c635e6b77807677741
RyanPliske <ryanpliske@yahoo.com>
2015-01-13 23:18:47 -0600

    Added the initial setup to deal with saving to and retrieving poems from Database

---
+++ js/index.js
@@ -0,0 +1,51 @@
+"use strict";
+var poemViewController = (function(){
+  // Create a request Object for Ajax
+  var xmlhttp;
+  if (window.XMLHttpRequest) // If browser supports XMLHttpRequest it's: IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp = new XMLHttpRequest();
+  else // IE6 or IE5 so use ActiveXObject
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
+  // Function to Submit to Database
+  var submitPoem = function() {
+    console.log("Saving poem to database...");
+    this.xmlhttp.open("POST", "poemModel.js", true);
+    this.xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    this.xmlhttp.send("poemName=RyansPoem&poemText=Onomotopoeia");
+  };
+  // Function to Retrieve From Database
+  var retrievePoem = function() {
+    console.log("Retrieving poem...");
+  };
+  // Make some Functions and Objects available to poemView
+  return{
+    submitPoem: submitPoem,
+    retrievePoem: retrievePoem,
+    xmlhttp: xmlhttp
+  };
+})();
+
+var poemView = (function () {
+  var submitPoemButton = document.getElementById("submitPoem");
+  var retrievePoemButton = document.getElementById("retrievePoem");
+  var resultsTextBox = document.getElementById('resultFromServer')
+  // Submit Poem to Postgres upon Click
+  submitPoemButton.onclick = function () {
+    poemViewController.submitPoem();
+  };
+  // Retrieve Poem from Postgres upon Click
+  retrievePoemButton.onclick = function () {
+    poemViewController.retrievePoem();
+  };
+
+  // Deal with the request
+  poemViewController.xmlhttp.onreadystatechange = function(){
+    // When finished
+    if (poemViewController.xmlhttp.readyState == 4 && poemViewController.xmlhttp.status == 200)
+    {
+      // Do something
+    }
+  }
+  
+})();
---
+++ js/poemViewController.js
@@ -0,0 +1,5 @@
+"use strict";
+module.exports.submitPoem = function (text, callback){
+	// Send request to server
+	return callback(null, "Successfully!");
+}
